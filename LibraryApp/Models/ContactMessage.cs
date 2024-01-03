7df0d8604d57f7c50f00c99088329aee9d53b4fa
emrullahkoc <150725735+emrullahkoc@users.noreply.github.com>
2024-01-03 22:09:19 +0300

    v0.01

---
+++ LibraryApp/Models/ContactMessage.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApp.Models
+{
+    public class ContactMessage
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(32)]
+        public string FirstName { get; set; }
+        [Required, StringLength(32)]
+        public string LastName { get; set; }
+        [Required, StringLength(32)]
+        public string Title { get; set; }
+        [Required, StringLength(256)]
+        public string Details { get; set; }
+        [Required]
+        public bool Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Read { get; set; }
+        public string? Reply { get; set; }
+    }
+}
---
+++ LibraryApp/Models/LibraryDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApp.Models
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext()
+        {
+
+        }
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<MainPage> MainPages { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ContactMessage> ContactMessages { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder
+            optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+            "Server=EMRULLAH\\SQLEXPRESS;Database=LibraryApp;" +
+            "TrustServerCertificate=True;Trusted_Connection=True;"
+               );
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Favorite>()
+                .HasKey(x => new { x.BookId, x.UserId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
