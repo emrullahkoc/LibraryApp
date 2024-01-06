fe32ea235f0f1af21dade28edc6df1246a0ecfce
emrullahkoc <150725735+emrullahkoc@users.noreply.github.com>
2024-01-06 12:51:58 +0300

    v00.3

--- LibraryApp/obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
+++ LibraryApp/obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0495c5b9b0fb12c80ec0ebe79ac563393e96e751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef84359fbadfd69e4cbe16ae9f848bb0f150cb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
